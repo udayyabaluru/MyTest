4b9165e8e1133d2a9689e8a689d55ff467c95c8f
udayyabaluru <161834410+udayyabaluru@users.noreply.github.com>
2024-03-15 15:16:31 +0530

    commiting few files
    main files

---
+++ src/main/java/com/identity/detection/IdentityLauncher.java
@@ -0,0 +1,11 @@
+package com.identity.detection;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class IdentityLauncher 
+{
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(IdentityLauncher.class, args);
+	}
+}
